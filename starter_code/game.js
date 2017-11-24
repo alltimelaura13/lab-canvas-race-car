ac3cc99860a8276ee70906fde94cb506d1547271
LAURA MARCOS AGUILAR <Laura@MacBook-Pro-de-LAURA.local>
2017-11-24 20:07:50 +0100

    hasta aquí la carretera

---
+++ starter_code/game.js
@@ -0,0 +1,30 @@
+function Game(canvasId) {
+  this.canvas = document.getElementById(canvasId);
+  this.ctx = this.canvas.getContext("2d");
+
+  this.car = new car(this.canvas, "/images/car.png");
+
+  // this.bars = [];
+  //
+  // setInterval(this.addBar.bind(this), 1000);
+}
+
+Game.prototype.isReady = function() {
+  return this.car.isReady();
+}
+//
+// Game.prototype.addBar = function() {
+//   this.bars.push(new Bar(this.canvas, 10, 100));
+}
+
+Game.prototype.draw = function() {
+  if (this.isReady()) {
+    this.clear();
+    this.drawBg();
+    this.car.draw();
+  }
+  window.requestAnimationFrame(this.draw.bind(this));
+}
+
+var game = new Game("canvas");
+game.draw();
--- starter_code/script.js
+++ starter_code/script.js
@@ -3,7 +3,56 @@ window.onload = function() {
     startGame();
   };
 
-  function startGame() {
 
+  function startGame() {
+    this.button.onclick = function () {
+
+    }
   }
 };
+
+
+
+
+
+
+function Canvas(id) {
+  var canvas = document.getElementById(id);
+  canvas.width = 500;
+  canvas.height = 800;
+  this.ctx = canvas.getContext("2d");
+}
+
+Canvas.prototype.drawRect = function(x, y, width, height, color) {
+  this.ctx.beginPath();
+  this.ctx.rect(x, y, width, height);
+  this.ctx.fillStyle = color;
+  this.ctx.fill();
+  this.ctx.closePath();
+}
+
+Canvas.prototype.drawSquare = function(x, y, width) {
+  this.drawRect(x, y, width, width);
+}
+
+
+
+Canvas.prototype.draw = function() {
+  this.drawRect(0, 10, 500, 1000, "rgb(0, 153, 0)");
+  this.drawRect(30, 10, 440, 800, "rgb(140, 140, 140)");
+  this.drawRect(40, 10, 420, 800, "rgb(255, 255, 255)");
+  this.drawRect(45, 10, 410, 800, "rgb(140, 140, 140)");
+  this.drawRect(245, 50, 10, 40, "rgb(255, 255, 255)");
+  this.drawRect(245, 150, 10, 40, "rgb(255, 255, 255)");
+  this.drawRect(245, 250, 10, 40, "rgb(255, 255, 255)");
+  this.drawRect(245, 350, 10, 40, "rgb(255, 255, 255)");
+  this.drawRect(245, 450, 10, 40, "rgb(255, 255, 255)");
+  this.drawRect(245, 550, 10, 40, "rgb(255, 255, 255)");
+  this.drawRect(245, 650, 10, 40, "rgb(255, 255, 255)");
+  this.drawRect(245, 750, 10, 40, "rgb(255, 255, 255)");
+
+}
+
+
+var canvas = new Canvas("canvas");
+canvas.draw();
