c9d48405c601814665249dfcf586e9364302fc20
LAURA MARCOS AGUILAR <Laura@MacBook-Pro-de-LAURA.local>
2017-11-25 11:10:25 +0100

    por fin he conseguido añadir el coche

--- starter_code/game.js
+++ starter_code/game.js
@@ -4,33 +4,5 @@ function Game(canvasId) {
 
   this.car = new car(this.canvas, "images/car.png");
 
-  // this.bars = [];
-  //
-  // setInterval(this.addBar.bind(this), 1000);
+
 }
-//
-// Game.prototype.isReady = function() {
-//   return this.car.isReady();
-// }
-//
-// Game.prototype.addBar = function() {
-//   this.bars.push(new Bar(this.canvas, 10, 100));
-// }
-//
-// Game.prototype.clear = function() {
-//   this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-// }
-//
-//
-//
-// Game.prototype.draw = function() {
-//   if (this.isReady()) {
-//     this.clear();
-//     this.drawBg();
-//     this.car.draw();
-//   }
-//   window.requestAnimationFrame(this.draw.bind(this));
-// }
-//
-// var game = new Game("canvas");
-// game.draw();
--- starter_code/script.js
+++ starter_code/script.js
@@ -19,10 +19,17 @@ window.onload = function() {
 
 
 function Canvas(id) {
-  var canvas = document.getElementById(id);
+  var canvas = document.getElementById('canvas');
   canvas.width = 500;
   canvas.height = 800;
   this.ctx = canvas.getContext("2d");
+
+  var img = new Image();
+  img.src = "./images/car.png";
+
+  img.onload = (function() {
+    this.ctx.drawImage(img, 179, 425, 50, 100);
+  }).bind(this);
 }
 
 Canvas.prototype.drawRect = function(x, y, width, height, color) {
